4679f44edde53e53f9ade59dd8bda8f41c4f7fc0
Nik1tk <89960168401super@gmail.com>
2026-05-20 00:32:51 +0300

    feat: set version 1.0

---
+++ calculator.py
@@ -0,0 +1,18 @@
+from math import*
+ver="1.0"
+a=int(input("Введите кэффициент а:"))
+b=int(input("Введите кэффициент b:"))
+c=int(input("Введите кэффициент c:"))
+if not isinstance(a, (int, float)) or not isinstance(b, (int, float)) or not isinstance(c, (int, float)):
+        raise TypeError("Arguments must be numbers")
+
+D=(b**2)-4*a*c
+if D>0:
+    x1=(-b-(sqrt(D)))/(2*a)
+    x2=(-b+(sqrt(D)))/(2*a)
+    print(x1,x2,(x2-x1))
+if D==0:
+    x=(-b)/(2*a)
+    print(x)
+if D<0:
+    print("Корней нет") 
