b7bdb25f4579df47a6fcec7a820f55da800954a6
Nik1tk <89960168401super@gmail.com>
2026-05-20 00:02:30 +0300

    feat: add add and subtract functions

---
+++ calculator.py
@@ -0,0 +1,14 @@
+from math import*
+a=int(input("Введите кэффициент а:"))
+b=int(input("Введите кэффициент b:"))
+c=int(input("Введите кэффициент c:"))
+D=(b**2)-4*a*c
+if D>0:
+    x1=(-b-(sqrt(D)))/(2*a)
+    x2=(-b+(sqrt(D)))/(2*a)
+    print(x1,x2)
+if D==0:
+    x=(-b)/(2*a)
+    print(x)
+if D<0:
+    print("Корней нет")
