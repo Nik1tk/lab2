945d98a69cb5eb716e74505a1e7c00fbb231ec01
Nik1tk <89960168401super@gmail.com>
2026-05-20 00:23:40 +0300

    fix: add type checking

--- calculator.py
+++ calculator.py
@@ -2,6 +2,8 @@ from math import*
 a=int(input("Введите кэффициент а:"))
 b=int(input("Введите кэффициент b:"))
 c=int(input("Введите кэффициент c:"))
+if not isinstance(a, (int, float)) or not isinstance(b, (int, float)) or not isinstance(с, (int, float)):
+        raise TypeError("Arguments must be numbers")
 D=(b**2)-4*a*c
 if D>0:
     x1=(-b-(sqrt(D)))/(2*a)
